b384decf540cd658508dc0a6ea81cc9531118add
sushka <r.m.poltorabatko@gmail.com>
2024-10-05 16:36:01 +0300

    🛠️ refactor(graph): изменил типы параметров на `&Vec` и добавил `clone`

--- src/graph.rs
+++ src/graph.rs
@@ -3,26 +3,26 @@ use chrono::Datelike;
 use egui_plot::PlotPoint;
 use crate::tg_result::{Actor, TgExportMessage};
 
-pub(crate) fn split_by_people(messages: impl Into<Vec<TgExportMessage>>) -> HashMap<Actor, Vec<TgExportMessage>> {
+pub(crate) fn split_by_people(messages: impl Into<&Vec<TgExportMessage>>) -> HashMap<Actor, Vec<TgExportMessage>> {
     let mut data: HashMap<Actor, Vec<TgExportMessage>> = HashMap::new();
     for message in messages.into() {
         let key = Actor::new(message.get_actor_id(), message.get_actor());
         if data.contains_key(&key) {
-            data.get_mut(&key).unwrap().push(message);
+            data.get_mut(&key).unwrap().push(message.clone());
         } else {
-            data.insert(key, vec![message]);
+            data.insert(key, vec![message.clone()]);
         }
     };
 
     data
 }
 
-pub(crate) fn count_by_days(messages_by_actor: impl Into<HashMap<Actor, Vec<TgExportMessage>>>) -> HashMap<Actor, Vec<PlotPoint>> {
+pub(crate) fn count_by_days(messages_by_actor: impl Into<&HashMap<Actor, Vec<TgExportMessage>>>) -> HashMap<Actor, Vec<PlotPoint>> {
     let mut data: HashMap<Actor, Vec<PlotPoint>> = HashMap::new();
     for (actor, mut messages) in messages_by_actor.into() {
         messages.sort_by_key(|m| { m.get_date().num_days_from_ce() });
         data.insert(
-            actor,
+            actor.into(),
             messages.chunk_by(
                 |a, b| {
                     a.get_date().num_days_from_ce() == b.get_date().num_days_from_ce()
