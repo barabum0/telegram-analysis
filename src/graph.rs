eb457002f0d29e52267c898d4f0b22e086f1fcd1
sushka <r.m.poltorabatko@gmail.com>
2024-10-05 16:30:15 +0300

    ✨ feat(graph): добавил подсчет сообщений по дням

--- src/graph.rs
+++ src/graph.rs
@@ -1,4 +1,6 @@
 use std::collections::HashMap;
+use chrono::Datelike;
+use egui_plot::PlotPoint;
 use crate::tg_result::{Actor, TgExportMessage};
 
 pub(crate) fn split_by_people(messages: impl Into<Vec<TgExportMessage>>) -> HashMap<Actor, Vec<TgExportMessage>> {
@@ -12,5 +14,21 @@ pub(crate) fn split_by_people(messages: impl Into<Vec<TgExportMessage>>) -> Hash
         }
     };
 
+    data
+}
+
+pub(crate) fn count_by_days(messages_by_actor: impl Into<HashMap<Actor, Vec<TgExportMessage>>>) -> HashMap<Actor, Vec<PlotPoint>> {
+    let mut data: HashMap<Actor, Vec<PlotPoint>> = HashMap::new();
+    for (actor, mut messages) in messages_by_actor.into() {
+        messages.sort_by_key(|m| { m.get_date().num_days_from_ce() });
+        data.insert(
+            actor,
+            messages.chunk_by(
+                |a, b| {
+                    a.get_date().num_days_from_ce() == b.get_date().num_days_from_ce()
+                }
+            ).collect(),
+        )
+    };
     data
 }
