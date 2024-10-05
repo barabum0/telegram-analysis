1cfc9d47292d4b9e9336fc04d7128b08eeae7552
sushka <r.m.poltorabatko@gmail.com>
2024-10-05 16:15:18 +0300

    ✨ feat: добавил функцию разделения сообщений по отправителям

---
+++ src/graph.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+use crate::tg_result::TgExportMessage;
+
+pub fn split_by_people(messages: impl Into<Vec<TgExportMessage>>) -> HashMap<(u64, String), Vec<TgExportMessage>> {
+    let mut data: HashMap<(u64, String), Vec<TgExportMessage>> = HashMap::new();
+    for message in messages.into() {
+        let key = (message.get_actor_id(), message.get_actor());
+        if data.contains_key(&key) {
+            data.get_mut(&key).unwrap().push(message);
+        } else {
+            data.insert(key, vec![message]);
+        }
+    };
+
+    data
+}
