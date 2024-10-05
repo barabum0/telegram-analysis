2e8642157fcca732ef8a1c40d024bde9b3128ee7
sushka <r.m.poltorabatko@gmail.com>
2024-10-05 16:15:25 +0300

    🔧 refactor(Cargo.toml): добавил features для serde и chrono

--- src/graph.rs
+++ src/graph.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 use crate::tg_result::TgExportMessage;
 
-pub fn split_by_people(messages: impl Into<Vec<TgExportMessage>>) -> HashMap<(u64, String), Vec<TgExportMessage>> {
+pub(crate) fn split_by_people(messages: impl Into<Vec<TgExportMessage>>) -> HashMap<(u64, String), Vec<TgExportMessage>> {
     let mut data: HashMap<(u64, String), Vec<TgExportMessage>> = HashMap::new();
     for message in messages.into() {
         let key = (message.get_actor_id(), message.get_actor());
