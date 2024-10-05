fcd296a9666d61c4a5b0bec23d05bb363a76e3e6
sushka <r.m.poltorabatko@gmail.com>
2024-10-05 16:22:55 +0300

    ✨ refactor(src/graph.rs): заменил тип ключа на `Actor` в `split_by_people`

--- src/graph.rs
+++ src/graph.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
-use crate::tg_result::TgExportMessage;
+use crate::tg_result::{Actor, TgExportMessage};
 
-pub(crate) fn split_by_people(messages: impl Into<Vec<TgExportMessage>>) -> HashMap<(u64, String), Vec<TgExportMessage>> {
-    let mut data: HashMap<(u64, String), Vec<TgExportMessage>> = HashMap::new();
+pub(crate) fn split_by_people(messages: impl Into<Vec<TgExportMessage>>) -> HashMap<Actor, Vec<TgExportMessage>> {
+    let mut data: HashMap<Actor, Vec<TgExportMessage>> = HashMap::new();
     for message in messages.into() {
-        let key = (message.get_actor_id(), message.get_actor());
+        let key = Actor::new(message.get_actor_id(), message.get_actor());
         if data.contains_key(&key) {
             data.get_mut(&key).unwrap().push(message);
         } else {
